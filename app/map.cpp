c4f033f9775ae5bfc476118400a88409e32849bd
Yash Shah <ysshah95@umd.edu>
2018-10-16 17:18:22 -0400

    [9:MAP] Implemented map class

--- app/map.cpp
+++ app/map.cpp
@@ -36,8 +36,69 @@
 #include "map.hpp"
 
 std::vector<std::vector<int>> map::create_map() {
+    // Initilaizing the map with 1s as a free space
+    std::vector<std::vector<int> > map_vec(800, std::vector<int>(800, 1));
+    int rectangle_center_x = 400;   // Rectangle center's x coordinate.
+    int rectangle_center_y = 400;   // Rectangle center's y coordinate.
+    int rectangle_span = 100;       // Span of Rectangle in all direction.
+
+    for (int i = rectangle_center_x - rectangle_span;
+                i < rectangle_center_x + rectangle_span; i++) {
+        for (int j = rectangle_center_y - rectangle_span;
+                    j < rectangle_center_y + rectangle_span; j++) {
+        map_vec[i][j] = 0; // updating the matrix elements to 0 if in obstacle.
+        }
+    }
+    map_vec[2][2] = 0;
+    return map_vec;
 }
 
 bool map::print_path(std::vector<std::vector<int>> map, 
                         std::vector<std::pair<int,int>> path) {
+    if (path[1].first == -1){
+        return 0;
+    }
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow(
+        "Testing od SDL",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        800,
+        SDL_WINDOW_SHOWN
+    );
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_SetRenderDrawColor (renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    // Set obstacle color to red
+    for(auto i=0; i<=799; i++){
+        for(auto j=0; j<=799; j++){
+            if (map[i][j] == 0){
+                SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                SDL_RenderDrawPoint(renderer, i, j);
+            }
+        }
+    }
+
+    // Set path colour to blue
+    for (auto i=0; i < path.size(); i++){
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        SDL_RenderDrawPoint(renderer, path[i].first, path[i].second);
+    }
+
+    //Update the screen
+    SDL_RenderPresent(renderer);
+
+    bool isPolling = true;
+    do{
+        SDL_Event event;
+        SDL_PollEvent(&event);
+        if (event.type == SDL_QUIT) {
+            isPolling = false;
+        }
+        SDL_Delay(0);
+    } while (isPolling);
+    return 1;
 }
